ca148371cde89f72f8f8ecb796c568a31d7566a8
Micah Hawkins <micahbhawkins@gmail.com>
2015-05-17 16:16:26 -0700

    implemented this

--- src/AddressBook.js
+++ src/AddressBook.js
@@ -1,11 +1,11 @@
 function AddressBook() {
-	var contacts = [];
+	this.contacts = [];
 
 	this.addContact = function(contact) {
-		contacts.push(contact);
+		this.contacts.push(contact);
 	}	
 
 	this.getContact = function(contact) {
-		return contacts[contact];
+		return this.contacts[contact];
 	}
 }
