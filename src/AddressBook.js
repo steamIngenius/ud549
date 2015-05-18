f6972fae213f7d00f44cff5cd2967cf4fdf3ce3d
Micah Hawkins <micahbhawkins@gmail.com>
2015-05-17 17:17:59 -0700

    Add async testing

--- src/AddressBook.js
+++ src/AddressBook.js
@@ -1,5 +1,18 @@
 function AddressBook() {
 	this.contacts = [];
+	this.initialComplete = false;
+
+	this.getInitialContacts = function(cb) {
+		var self = this
+
+		// fake some asynchronicity
+		setTimeout(function() {
+			self.initialComplete = true;
+			if (cb) {
+				return cb();
+			}
+		}, 3);
+	}
 
 	this.addContact = function(contact) {
 		this.contacts.push(contact);
@@ -7,5 +20,9 @@ function AddressBook() {
 
 	this.getContact = function(contact) {
 		return this.contacts[contact];
+	}
+
+	this.deleteContact = function(contact) {
+		this.contacts.splice(contact, 1);	
 	}
 }
